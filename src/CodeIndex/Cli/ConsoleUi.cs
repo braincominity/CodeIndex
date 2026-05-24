1dfd20bd82ef2e3bd945008ac67a0afc966e364f
Widthdom <widthdom@gmail.com>
2026-05-24 19:53:05 +0900

    Fix spinner async delay for issue #1421

--- src/CodeIndex/Cli/ConsoleUi.cs
+++ src/CodeIndex/Cli/ConsoleUi.cs
@@ -221,7 +221,7 @@ public static class ConsoleUi
 
         var cts = new CancellationTokenSource();
         var ct = cts.Token;
-        Task.Run(() =>
+        _ = Task.Run(async () =>
         {
             int i = 0;
             while (!ct.IsCancellationRequested)
@@ -234,7 +234,7 @@ public static class ConsoleUi
                     Console.Out.Flush();
                 }
                 i++;
-                try { Task.Delay(SpinnerFrameDelayMs, ct).Wait(ct); } catch (OperationCanceledException) { break; }
+                try { await Task.Delay(SpinnerFrameDelayMs, ct).ConfigureAwait(false); } catch (OperationCanceledException) { break; }
             }
         }, ct);
         return cts;
